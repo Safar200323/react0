055815ee4f9e39e20ef8ef8236babacd7f2a140b
Safar200323 <142076988+Safar200323@users.noreply.github.com>
2023-09-10 18:51:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {
+    count: 0,
+  };
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Counter App</h1>
+        <div className="counter">
+          <button onClick={this.decrement}>-</button>
+          <span>{this.state.count}</span>
+          <button onClick={this.increment}>+</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
